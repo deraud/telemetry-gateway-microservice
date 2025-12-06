e370146e18bc5582925d2ccb9b8fa420ed58f865
deraud <audric.derren@gmail.com>
2025-12-06 17:15:26 +0700

    feat: WS Authentication (API key)

--- src/websocket/ws.ts
+++ src/websocket/ws.ts
@@ -4,10 +4,22 @@ import { Server } from "http";
 export function createWebSocketServer(httpServer: Server) {
   const wss = new WebSocketServer({ server: httpServer, path: "/ws" });
 
-  wss.on("connection", (ws) => {
-    console.log("New WebSocket client connected");
+  wss.on("connection", (ws, request) => {
+    const url = new URL(request.url!, `http://${request.headers.host}`);
+    const apiKey = url.searchParams.get("api_key");
+    console.log(url.toString());
+    if (apiKey !== process.env.API_KEY) {
+      console.log("WebSocket authentication failed");
 
-    ws.send(JSON.stringify({ message: "Connected to telemetry gateway WS" }));
+      ws.send(JSON.stringify({ message: "WebSocket authentication failed" }));
+
+      ws.close();
+      return;
+    }
+
+    console.log("WebSocket authentication successful");
+
+    ws.send(JSON.stringify({ message: "Authenticated WebSocket client" }));
 
     ws.on("close", () => {
       console.log("WebSocket client disconnected");
